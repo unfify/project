a9580f08ac1516a3c1e4954a2d303aadf1288201
Petr <nesterovpetr6@gmail.com>
2021-11-19 07:57:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+//Переменные,типы данных
+//let number = 4.6;
+//console.log(4 / 0);
+//console.log('string' * 9);
+//const person = "Alex";
+//const bool = false;
+//console.log(something);
+//let und;
+//console.log(und);
+//const obj = {
+    //  name: "John",
+      //age: 25,
+//    isMarried: false,
+//};
+//console.log(obj["name"]);
+//let arr = ['plum.png', 'apple.bmp', 'orange.jpg'];
+//console.log(arr[1]);
+
+
+//Просто взаимодействие с пользователем
+//const result = confirm("Are you here?");
+//console.log(result);
+//const answer = prompt("Вам есть 18?");
+//console.log(typeof(answer));
+//const answers = [];
+//answers[0] = prompt("Как ваше имя?", '');
+//answers[1] = prompt("Фамилия", '');
+//answers[2] = prompt("Сколько вам лет", '');
+//console.log(typeof (answers));
+
+//Интерполяция.
+//const category = 'toys';
+//console.log(`https://someurl.com/${category}/5`);
+//const user = "Ivan";
+//alert(`Привет,${user}`);
+
+
+//Операторы в JS
+//console.log('arr' + "-object");
+//console.log(4 + + "5");
+//let incr = 10,
+//    decr = 10;
+//incr++;
+//decr--;
+//console.log(++incr);
+//console.log(--decr);
+//console.log(5 % 2);
+//console.log(2 * 4 === '8');
+//const isChecked = true,
+//    isClose = false;
+//console.log(isChecked || isClose);
+
+//Система контроля версий git и сервис github 
